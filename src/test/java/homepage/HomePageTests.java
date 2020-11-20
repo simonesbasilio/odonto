6d089459ffdd2af471181db4df7d7763cb1c44ce
simonesbasilio <67930696+simonesbasilio@users.noreply.github.com>
2020-11-20 12:23:48 -0300

    up

---
+++ src/test/java/homepage/HomePageTests.java
@@ -0,0 +1,15 @@
+package homepage;
+
+import org.junit.jupiter.api.Test;
+
+import base.BaseTests;
+
+public class HomePageTests extends BaseTests {
+	
+	
+	@Test
+	public void testCarregarPaginaInicial() {
+		carregarPaginaInicial();
+	}
+
+}
